b78751d6b2c013c275aa83b2e0dd5b276a4a2e48
baddala <baddala156@gmail.com>
2018-04-07 08:34:19 +0530

    ccc

---
+++ company-list.php
@@ -0,0 +1,39 @@
+<?php
+include_once('confi.php');
+
+
+ 
+ // get data into data base
+$sql = "SELECT * FROM company";
+
+ $result = mysqli_query($conn,$sql);
+ $rows = array();
+  $res = array();
+
+ if ($result) {
+ if (mysqli_num_rows($result) > 0) {
+ while($row = $result->fetch_array()){
+    $json1['id'] = $row['id'];
+    $json1['companyName'] = $row['company_name'];
+	$json1['description'] = $row['description'];
+	 
+	$rows[] = $json1;
+}
+$json = array("status" => 1, "msg" => "Success",'companyList'=>$rows);
+ }else{
+	 $json = array("status" => 1, "msg" => "No companys available",'companyList'=>$rows);
+ }
+ }else{
+	 $json = array("status" => 1, "msg" => "No companys available",'companyList'=>$rows);
+ }
+
+ 
+
+
+mysqli_close($conn); 
+
+/* Output header */
+ header('Content-type: application/json');
+echo json_encode($json);
+ //echo json_encode($json1);
+?>
