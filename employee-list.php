8884e793301913f4e9dcd096104f4163720e3b35
kishan530 <kishan.kish530@gmail.com>
2018-04-13 22:52:30 +0530

    New api for employee

--- employee-list.php
+++ employee-list.php
@@ -6,7 +6,7 @@ $data = json_decode(file_get_contents('php://input'), true);
 $office = isset($data['office']) ? mysqli_real_escape_string($conn,$data['office']) : 0;
  
  // get data into data base
-$sql = "SELECT * FROM company";
+$sql = "SELECT * FROM users";
 
 if($office>0)
 $sql .= " where office_id=$office";
@@ -18,22 +18,22 @@ $sql .= " where office_id=$office";
  if (mysqli_num_rows($result) > 0) {
  while($row = $result->fetch_array()){
     $json1['id'] = $row['id'];
-    $json1['companyName'] = $row['company_name'];
-	$json1['description'] = $row['description'];
-	$json1['joiningDate'] = $row['joining_date'];
-	$json1['totalMonthlyRent'] = $row['Total_monthly_rent'];
-	$json1['companyType'] = $row['company_type'];
+    $json1['name'] = $row['name'];
+	$json1['email'] = $row['email'];
+	$json1['mobile'] = $row['mobile'];
+	$json1['status'] = (int) $row['status'];
+	$json1['role'] = $row['role'];
 	
 	 
 	$rows[] = $json1;
 }
 
-$json = array("status" => 1, "msg" => "Success",'companyList'=>$rows);
+$json = array("status" => 1, "msg" => "Success",'employeeList'=>$rows);
  }else{
-	 $json = array("status" => 1, "msg" => "No companys available",'companyList'=>$rows);
+	 $json = array("status" => 1, "msg" => "No companys available",'employeeList'=>$rows);
  }
  }else{
-	 $json = array("status" => 1, "msg" => "No companys available",'companyList'=>$rows);
+	 $json = array("status" => 1, "msg" => "No companys available",'employeeList'=>$rows);
  }
 
  
