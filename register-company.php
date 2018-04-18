99734c734a700aba528b82f6b34f89a373854dba
kishan <kishan.kish530@gmail.com>
2018-04-18 12:42:04 +0530

    updated

--- register-company.php
+++ register-company.php
@@ -11,16 +11,18 @@ $description = isset($data['Description']) ? mysqli_real_escape_string($conn,$da
 $joining_date = isset($data['joiningDate']) ? mysqli_real_escape_string($conn,$data['joiningDate']) : "";
 $Total_monthly_rent = isset($data['Tmrent']) ? mysqli_real_escape_string($conn,$data['Tmrent']) : "";
 $officeId = (int) isset($data['officeId']) ? mysqli_real_escape_string($conn,$data['officeId']) : 0;
-$file = isset($data['file']) ? mysqli_real_escape_string($conn,$data['file']) : "";
+$file = isset($data['logo']) ? $data['logo'] : "";
+$companySeats = isset($data['typesofseats']) ? $data['typesofseats'] : "";
 
 //echo (file_get_contents('php://input'));
 //echo var_dump($data);
 //$UserName = $data['UserName'];
 
-$file =   $_FILES;
+//$file =   $_FILES;
 
-//echo var_dump($file);
-//exit();
+echo var_dump($file);
+echo var_dump($_FILES);
+exit();
 
  $json1 = array("CompanyName" => $CompanyName , "description" => $description, "joining_date" => $joining_date, "Total_monthly_rent" => $Total_monthly_rent, "company_type" => $CompanyType,"officeId"=>$officeId);
  // Insert data into data base
@@ -29,6 +31,19 @@ $sql = "INSERT INTO company (company_name, description, joining_date,Total_month
    $id = $conn->insert_id;
  $json1['companyId'] = $id;
  if($qur){
+ foreach($companySeats as $companySeat){
+// echo var_dump($companySeat);
+//exit();
+ $typesOfSeat =  isset($companySeat['typesOfSeats']) ? mysqli_real_escape_string($conn,$companySeat['typesOfSeats']) : '';
+  $numberofseats = (int) isset($companySeat['numberofseats']) ? mysqli_real_escape_string($conn,$companySeat['numberofseats']) : 0;
+  $companySeatSql = " INSERT INTO company_seats ( type_of_seats, no_of_seats,company_id) VALUES ('" . $typesOfSeat . "', '" . $numberofseats . "', '" . $id . "');";
+  $copanyQur = $conn->query($companySeatSql);
+   if($copanyQur){
+   // $json = array("status" => 0, "msg" => "Error adding register company!". $conn->error);
+   }else{
+    $json = array("status" => 0, "msg" => "Error adding register company!". $conn->error);
+   }
+ }
  $json = array("status" => 1, "msg" => "Done register company added!",'details'=>$json1);
  }else{
  $json = array("status" => 0, "msg" => "Error adding register company!". $conn->error);
