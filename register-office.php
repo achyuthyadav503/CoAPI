fbb8e957ef1f95f18ebf5e28162077c715c1ee8f
baddala <baddala156@gmail.com>
2018-04-28 23:03:40 +0530

    coapi

--- register-office.php
+++ register-office.php
@@ -54,6 +54,41 @@ $sql = "INSERT INTO register_office (OfficeName,description, Address, Location,C
    }
  }
  
+ $to      = "baddala.venugopalreddy@gmail.com";
+
+                                     $subject = "Registration success";
+                                     $message ="Your registration has been successfull.
+
+                                            Please click on the following link to activate you account.
+
+
+															Regards,
+															Team Cospaze";
+                                    $headers = "From: " .$email. "\r\n";
+                                    $headers .= "Reply-To: ".$email. "\r\n";
+                                    
+                                    $headers .= "MIME-Version: 1.0\r\n";
+                                    $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+                                    
+                                    $messageBody .= '<body>
+                                    <div class="div2" style=" width: 609px;  padding: 50px; background-color:#CCC;">
+                                                                        <div class="div1" style=" background-color:white; border: 1px solid white;  margin-left: 30px; width: 550px; font-size:14px;">
+                                                                                
+                                                                                
+                                          <h1>Contact Person  Message</h1>
+                                          
+										  <p>
+                                         <b> subject </b> :'. $subject .' </p>
+                                          <p>
+                                        <b>message </b> : '. $message .' </p>';
+                                            
+                                            $messageBody .=' 
+                                            </div>
+                                    </div>
+                                    </body>';
+                              
+                              mail($to, $subject, $messageBody, $headers);
+ 
  
  
  $json = array("status" => 1, "msg" => "Done Office added!",'details'=>$json1);
